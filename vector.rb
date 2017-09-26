6bfbbfdffcbb5056c4108739321b166ef91c1e3c
j1304 <j1304@PC4-51.cc.matsue-ct.jp>
2017-09-26 11:41:40 +0900

    file commit

---
+++ vector.rb
@@ -0,0 +1,20 @@
+class Vector
+ attr_accessor :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def to_s
+    "(#{@x}, #{@y})"
+  end
+
+  def length
+    Math.sqrt(@x**2 + @y**2)
+  end
+
+  def add(vec)
+   "(#{vec.x+@x}, #{vec.y+@y})"
+  end
+end
